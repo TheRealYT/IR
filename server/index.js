e07f3cd6b3a99c1248507ef2f340f2281c724212
TheRealYT <yt040448@gmail.com>
2024-05-26 10:42:26 +0300

    updated

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 
 const processor = require('./api/processor');
+const search = require('./api/search');
 
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({limit: '500mb'}));
 app.use(processor);
+app.use(search);
 
 app.listen(PORT, () => {
     console.log('Server started ', `http://127.0.0.1:${PORT}`);
