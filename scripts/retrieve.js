303c8660066c9c7cd01e7451c0fa58bf12b2bdb2
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2024-05-28 21:53:10 +0300

    fix

--- scripts/retrieve.js
+++ scripts/retrieve.js
@@ -17,9 +17,10 @@ async function retrieve(q) {
     const terms = tokenize(q);
     const queryFreq = {};
 
-    await normalize(terms);
-    //stem(terms);
     freq(terms, queryFreq);
+
+    await normalize(queryFreq);
+    //stem(queryFreq);
 
     const index = JSON.parse((await fss.readFile(INDEX_PATH)).toString());
     const uniqueTerms = Object.keys(queryFreq);
